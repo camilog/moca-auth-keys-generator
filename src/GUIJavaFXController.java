4d55e77b339c35f82eee9f87efe71b85a33928e6
Camilo Gomez <cjgomez@autistici.org>
2015-09-01 18:20:48 -0300

    Siguiendo implementación de JavaFX

--- src/GUIJavaFXController.java
+++ src/GUIJavaFXController.java
@@ -1,15 +1,21 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GUIJavaFXController {
 
+    @FXML private Label bulletin_board_label;
+    @FXML private TextField new_bb_address_textfield;
+
     public void handleConfigureBBAddressButtonAction(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("GUIFX/configureBB.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("configureBB.fxml"));
         Stage stage = new Stage();
         stage.setTitle("Configure Bulletin Board Address");
         stage.setScene(new Scene(root, 400, 200));
@@ -30,7 +36,11 @@ public class GUIJavaFXController {
 
     }
 
-    public void handleNewBBAddressButtonAction(ActionEvent actionEvent) {
+    @FXML public void handleNewBBAddressButtonAction(ActionEvent actionEvent) {
+
+        System.out.println("You clicked me!");
+        actionEvent.getSource()
+
 
     }
 }
